2944388610928eec1f0709b9a51db86511e8f328
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-09-16 03:36:16 -0400

    completed the productTag, Tag and Index files in Models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,12 +1,14 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
+const Category = require("./Category"); // import the Category model
+
 Product.init(
   {
     // define columns
@@ -23,28 +25,28 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      validate: {isDecimal: true}, // validates if the value is decimal
+      validate: { isDecimal: true }, // validates if the value is decimal
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10, // sets default value to 10
-      validate: {isNumeric: true}, // validates that the value is exclusively a number
-  },
-  category_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Category, // Refers to the Category model
-      key: 'id', // Refers to the id field in Category model
+      validate: { isNumeric: true }, // validates that the value is exclusively a number
     },
-  }
-},
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category, // Refers to the Category model
+        key: "id", // Refers to the id field in Category model
+      },
+    },
+  },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,38 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+
+const Tag = require('./Tag'); // import the Tag model
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+    Product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product, // Refers to the Product model
+        key: 'id', // Refers to the id field in Product model
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag, // Refers to the Tag model
+        key: 'id', // Refers to the id field in Tag model
+      },
+    }
   },
   {
     sequelize,
